d2e3bc5114967f347bd4470f6721462e539587ac
ttrine <trine.tyler@gmail.com>
2016-02-01 12:46:28 -0500

    Update valueNet.lua

--- valueNet.lua
+++ valueNet.lua
@@ -8,13 +8,13 @@ k = 192
 net = nn.Sequential()
 -- layer 1
 net:add(nn.SpatialZeroPadding(2,2,2,2))      -- pad 48x19x19 tensor into 48x23x23 tensor
-net:add(nn.SpatialConvolution(49, k, 5, 5))  -- convolve: #filters = k, size = 5x5, stride = 1x1
+net:add(nn.SpatialConvolution(49, k, 5, 5))  -- convolve. #filters = k, size = 5x5, stride = 1x1
 net:add(nn.ReLU())
 
 -- layers 2-12
 for i=2,12 do
   net:add(nn.SpatialZeroPadding(1,1,1,1))    -- pad kx19x19 tensor into kx21x21 tensor
-  net:add(nn.SpatialConvolution(k, k, 3, 3)) -- convolve: #filters = k, size = 3x3, stride = 1x1
+  net:add(nn.SpatialConvolution(k, k, 3, 3)) -- convolve. #filters = k, size = 3x3, stride = 1x1
   net:add(nn.ReLU())
 end
 
