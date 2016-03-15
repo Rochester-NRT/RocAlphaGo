755e8bea9d9d6908baf8b44302be69545ee51f98
James Tauber <jtauber@jtauber.com>
2016-03-15 18:04:21 -0400

    fixed flake8 violations (except W191, E266 and E501) in AlphaGo

--- AlphaGo/models/game_converter.py
+++ AlphaGo/models/game_converter.py
@@ -1,35 +1,37 @@
-import os, argparse
+import argparse
+import os
 import numpy as np
 from AlphaGo.models.preprocessing import Preprocess
 import AlphaGo.go as go
 from sgflib.sgflib import SGFParser, GameTreeEndError
 import cPickle as pickle
 
+
 class game_converter:
     def __init__(self):
-        self.index_at = {'a':0,'b':1,'c':2,'d':3,
-                         'e':4,'f':5,'g':6,'h':7,
-                         'i':8,'j':9,'k':10,'l':11,
-                         'm':12,'n':13,'o':14,'p':15,
-                         'q':16,'r':17,'s':18}
+        self.index_at = {'a': 0, 'b': 1, 'c': 2, 'd': 3,
+                         'e': 4, 'f': 5, 'g': 6, 'h': 7,
+                         'i': 8, 'j': 9, 'k': 10, 'l': 11,
+                         'm': 12, 'n': 13, 'o': 14, 'p': 15,
+                         'q': 16, 'r': 17, 's': 18}
 
     # convert move into board indices
-    def parse_raw_move(self,raw_move):
+    def parse_raw_move(self, raw_move):
         pos = list(str(raw_move)[3:5])
         y = self.index_at[pos[0]]
         x = self.index_at[pos[1]]
-        return (x,y)
+        return (x, y)
 
     # convert indices into 19x19 training label
-    def encode_label(self,move):
+    def encode_label(self, move):
         # convert move to one-hot encoding
-        one_hot = np.zeros((19,19),dtype=bool)
-        one_hot[move[0],move[1]] = 1
+        one_hot = np.zeros((19, 19), dtype=bool)
+        one_hot[move[0], move[1]] = 1
         return one_hot
 
     # convert full game into training samples
     def convert_game(self, file_name, features=None):
-        with open(file_name,'r') as file_object:
+        with open(file_name, 'r') as file_object:
             sgf_object = SGFParser(file_object.read())
         c = sgf_object.parse().cursor()
         tensors = []
@@ -55,9 +57,10 @@ class game_converter:
     def batch_convert(self, folder_path, features=None):
         file_names = os.listdir(folder_path)
         for file_name in file_names:
-            if file_name[-4:] != '.sgf': continue
+            if file_name[-4:] != '.sgf':
+                continue
             print file_name
-            training_samples = self.convert_game(os.path.join(folder_path,file_name), features)
+            training_samples = self.convert_game(os.path.join(folder_path, file_name), features)
             for sample in training_samples:
                 yield sample
 
@@ -69,9 +72,11 @@ if __name__ == '__main__':
 
     converter = game_converter()
     file_num = 0
-    for s_a_tuple in converter.batch_convert(args.infolder,
-        features=["board", "ones", "turns_since", "liberties", "capture_size",
-        "self_atari_size", "liberties_after","sensibleness", "zeros"]):
+    for s_a_tuple in converter.batch_convert(
+        args.infolder,
+        features=[
+            "board", "ones", "turns_since", "liberties", "capture_size",
+            "self_atari_size", "liberties_after", "sensibleness", "zeros"]):
         file_name = str(hash(s_a_tuple[1])) + "_" + str(file_num)
-        pickle.dump(s_a_tuple, open(os.path.join(args.outfolder,file_name), "wb"))
+        pickle.dump(s_a_tuple, open(os.path.join(args.outfolder, file_name), "wb"))
         file_num += 1
