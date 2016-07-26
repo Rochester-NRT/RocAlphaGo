48cb2e3f022b7ca47aaeaea730053e1d5b60af44
Robert Waite <rwaite@mobilityware.com>
2016-07-26 12:43:47 -0700

    Typo

--- AlphaGo/training/reinforcement_policy_trainer.py
+++ AlphaGo/training/reinforcement_policy_trainer.py
@@ -68,9 +68,9 @@ def make_training_pairs(player, opp, features, mini_batch_size, board_size=19):
 	winners = list()
 	for st in states:
 		if player_color == go.BLACK:
-			winner.append(st.get_winner())
+			winners.append(st.get_winner())
 		else:
-			winner.append(-1 * st.get_winner())
+			winners.append(-1 * st.get_winner())
 	# Concatenate tensors across turns within each game
 	for i in xrange(mini_batch_size):
 		X_list[i] = np.concatenate(X_list[i], axis=0)
