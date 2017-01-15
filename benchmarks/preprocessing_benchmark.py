1dec91a6a728ecc7f005abf4e0d65c9dc333dc1d
Paul Prescod <paul@prescod.net>
2017-01-15 07:15:11 -0800

    Fix wrong classname.

--- benchmarks/preprocessing_benchmark.py
+++ benchmarks/preprocessing_benchmark.py
@@ -1,18 +1,22 @@
-from AlphaGo.preprocessing.game_converter import game_converter
+import os, sys
+p = os.path
+parentddir = p.abspath(p.join(p.dirname(__file__), ".."))
+sys.path.append(parentddir)
+
+from AlphaGo.preprocessing.game_converter import GameConverter
 from cProfile import Profile
 
 prof = Profile()
 
 test_features = ["board", "turns_since", "liberties", "capture_size", "self_atari_size",
                  "liberties_after", "sensibleness", "zeros"]
-gc = game_converter(test_features)
+gc = GameConverter(test_features)
 args = ('tests/test_data/sgf/Lee-Sedol-vs-AlphaGo-20160309.sgf', 19)
 
 
 def run_convert_game():
     for traindata in gc.convert_game(*args):
         pass
-
 
 prof.runcall(run_convert_game)
 prof.dump_stats('bench_results.prof')
